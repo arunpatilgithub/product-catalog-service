c4cd621596582b0729c728df246e83aaea2a4eda
Arun Patil <arun.patil@lumen.com>
2023-01-26 13:05:17 -0700

    Added following -
    1. Models for Book
    2. Persistence
    3. Rest Controller for Product Catalog
    4. Test cases

---
+++ src/main/java/com/ap/productcatalogservice/dao/BookRepository.java
@@ -0,0 +1,16 @@
+package com.ap.productcatalogservice.dao;
+
+import com.ap.productcatalogservice.model.Book;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface BookRepository {
+
+    Collection<Book> findAll();
+    Optional<Book> findByIsbn(String isbn);
+    boolean existsByIsbn(String isbn);
+    Book save(Book book);
+    void deleteByIsbn(String isbn);
+
+}
---
+++ src/main/java/com/ap/productcatalogservice/exception/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ap.productcatalogservice.exception;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+
+}
