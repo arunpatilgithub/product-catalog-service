b408a5d71eff47b4156df3525337debfe73b7b1c
Arun Patil <89706905+arunpatilgithub@users.noreply.github.com>
2023-01-27 13:47:02 -0700

    Config service support (#2)
    added support for config service

--- src/main/java/com/ap/productcatalogservice/rest/HomeController.java
+++ src/main/java/com/ap/productcatalogservice/rest/HomeController.java
@@ -1,13 +1,20 @@
 package com.ap.productcatalogservice.rest;
 
+import com.ap.productcatalogservice.config.AppProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final AppProperties properties;
+
+    public HomeController(AppProperties properties) {
+        this.properties = properties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the book catalog!";
+        return properties.getGreeting();
     }
 }
